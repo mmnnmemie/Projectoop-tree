621dd6f5138cc982e82a66ba48ed790169e4c80b
mmnnmemie <72700844+mmnnmemie@users.noreply.github.com>
2020-11-13 17:16:56 +0700

    Add files via upload

---
+++ jTree1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtree;
+
+/**
+ *
+ * @author Hewy
+ */
+class jTree1 {
+
+    void setRootVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
